cd981535bf01b694bb38bee280e28b3401d2805c
Nojands <luisjtl86@gmail.com>
2023-12-21 15:26:51 -0600

    adding files

---
+++ proyecto1.py
@@ -0,0 +1,8 @@
+# print("Hola " +input("Cual es tu nombre y apellido?: " + " " + "Con base en tus respuestas,\nel nombre de tu cerveza seria: " + " input("Dime que color es el que mas te gusta?: ")
+# input("Cual es la temporada del año que mas te gusta?: ")
+# input("Dime tu super poder favorito: ")
+#
+# print(f/"Hola , \"{nombre},{apellido})\"")
+
+print(f"Hola {input('Cual es tu nombre?: ')} {input('Cual es tu apellido?: ')} con base en tus respuestas el nombre comercial de de tu cerveza"
+      f" seria, {input('Cual es tu animal favorito?: ')} {input('Cual es tu color favorito?: ')} de {input('Cual es tu estacion del año favorita?: ')}")
---
+++ strings.py
@@ -0,0 +1,18 @@
+print('100 + 50 ')
+
+print('Hola ' + 'Luis')
+
+print("Me llamo \"Luis\"")
+
+print("Esta es una linea\nY esta es otra linea")
+print("\t\tEsta es la tercera linea")
+
+print("Esta es una \\ barra invertida")
+
+print("Linea 1:\nLinea 2:\nLinea 3:")
+
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+print("Barra normal: /" )
+print("Barra invertida: \\")
+
