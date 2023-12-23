09d5b73bddbd4cd69af1308cd1577c9c26cb4374
Nojands <luisjtl86@gmail.com>
2023-12-23 00:19:00 -0600

    added a file

---
+++ tipos-datos.py
@@ -0,0 +1,23 @@
+# there are many data types
+#
+# e.g
+#
+# string (str) = any type of characters, could be letters, numbers or signs
+#
+# integer(int) = any number could be an integer
+#
+# floating(float) = are number with decimals
+
+listas(list) = could be strings, numbers, floats, or even list within the list
+
+diccionarios (dic) = are defined by a key and a value
+
+tuples (tuple) = are very similar to list, however its data is inmutable
+
+sets(set) = this is a group of unique values
+
+booleanos(bool) = they can only be true or false
+
+
+
+
